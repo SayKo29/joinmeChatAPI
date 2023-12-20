67accc3a9d6bb438994ddde7db2aa4d13779840c
SayKo29 <aaronsar29@gmail.com>
2023-12-20 18:36:04 +0100

    pruebas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
+    transports: ["websocket", "polling"],
     reconnectionDelay: 1000, // 1 segundo
     reconnectionAttempts: 10, // Número máximo de intentos
 });
